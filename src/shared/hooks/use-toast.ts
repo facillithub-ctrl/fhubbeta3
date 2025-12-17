d340d5f1c2f5fd7d7d0bd58c6619aef6d3b0c831
Pedro Jean <pedrojean877@gmail.com>
2025-12-17 01:47:30 -0300

    asdsadasd

---
+++ src/shared/hooks/use-toast.ts
@@ -0,0 +1,46 @@
+import * as React from "react"
+
+export type ToastActionElement = React.ReactNode
+
+export type ToastProps = {
+  id: string
+  title?: string
+  description?: string
+  action?: ToastActionElement
+  variant?: "default" | "destructive"
+  open?: boolean
+  onOpenChange?: (open: boolean) => void
+}
+
+type ToastArgs = Omit<ToastProps, "id">
+
+export function useToast() {
+  const [toasts, setToasts] = React.useState<ToastProps[]>([])
+
+  const toast = ({ ...props }: ToastArgs) => {
+    const id = Math.random().toString(36).substr(2, 9)
+    const newToast: ToastProps = { id, ...props, open: true }
+    
+    setToasts((prev) => [...prev, newToast])
+
+    return {
+      id,
+      dismiss: () => dismiss(id),
+      update: (props: ToastArgs) => {
+        setToasts((prev) => prev.map((t) => (t.id === id ? { ...t, ...props } : t)))
+      },
+    }
+  }
+
+  const dismiss = (id?: string) => {
+    setToasts((prev) => 
+      id ? prev.filter((t) => t.id !== id) : []
+    )
+  }
+
+  return {
+    toast,
+    toasts,
+    dismiss,
+  }
+}
